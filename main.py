a5433aeaf3476e2a0175906e616ffe615f9fd518
Bertan Günyel <bertan.gunyel@gmail.com>
2024-08-15 12:17:37 +0300

    Markdown experiments

--- main.py
+++ main.py
@@ -1,19 +1,25 @@
+from markdown_it.token import Token
 from rich import box
 from rich.columns import Columns
 from rich.console import Console
 from rich.panel import Panel
 from rich.tree import Tree
+from rich.markdown import ImageItem, Markdown, Heading
+from markdown_it.token import Token
 
 console = Console(record=True, width=100)
 
-about = """I'm a Data Scientist / Machine Learning Engineer with 2x M.Sc. degrees. I worked primarily on 
-Computer Vision problems in the past. Currently, I'm more interested in Natural Language Processing and Time-Series applications. 
+## PANEL 1
+
+about = """I'm a Data Scientist / Machine Learning Engineer with 2x M.Sc. degrees. I worked primarily on Computer Vision problems in the past. Currently, I'm more interested in Natural Language Processing and Time-Series applications. 
 
 You can find me on [bold link=https://twitter.com/bertan_gunyel]Twitter[/] and [bold link=https://www.linkedin.com/in/bertan-gunyel/]Linkedin"""
 
-panel = Panel.fit(
-    about, box=box.DOUBLE, border_style="blue", title="[b]Hello everyone", width=60
+panel1 = Panel.fit(
+    about, box=box.DOUBLE, border_style="blue", title="[b]Hello everyone", width=90
 )
+
+## PANEL 2
 
 tree = Tree("[link=https://www.linkedin.com/in/bertan-gunyel/]Bertan Günyel", guide_style="bold")
 projects_tree = tree.add(":open_file_folder: Projects", guide_style="bold")
@@ -55,11 +61,35 @@ tech_stack_cloud_tree.add('⭐ AWS')
 tech_stack_cloud_tree.add('⭐ GCP')
 tech_stack_cloud_tree.add('⭐ Azure')
 
-
-
 panel2 = Panel.fit(tree, box=box.DOUBLE, border_style="blue", title="[b]Technical Breakdown", width=80)
 
-console.print(Columns([panel, panel2]))
+## PANEL 3
+
+
+
+# panel3 = Panel.fit(img, box=box.DOUBLE, border_style="blue", title="[b]Github Stats", width=80)
+
+MARKDOWN = """
+# This is an h1
+
+Rich can do a pretty *decent* job of rendering markdown.
+
+1. This is a list item
+2. This is another list item
+
+This is an image
+![encoder-decoder](https://miro.medium.com/max/700/1*62xsdc5F5DNdLXluQojeBg.png)
+"""
+
+md = Markdown(MARKDOWN)
+
+
+
+
+# console.print(md)
+console.print(Columns([panel1, panel2, md]))
+
+
 
 CONSOLE_HTML_FORMAT = """\
 <pre style="font-family:Menlo,'DejaVu Sans Mono',consolas,'Courier New',monospace">{code}</pre>
