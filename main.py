ff8ddcd8d74b804a9d1803c1b1325894ada81af1
Bertan Günyel <bertan.gunyel@gmail.com>
2024-09-24 11:41:12 +0300

    Update

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ console = Console(record=True, width=100)
 
 ## PANEL 1
 
-about = """I'm a Data Scientist / Machine Learning Engineer with 2x M.Sc. degrees. I worked primarily on Computer Vision problems in the past. Currently, I'm more interested in Natural Language Processing and Time-Series applications. 
+about = """I'm a Data Scientist / Machine Learning Engineer with 2x M.Sc. degrees. I have worked primarily on Computer Vision, Natural Language Processing, and Time-Series problems 
 
 You can find me on [bold link=https://twitter.com/bertan_gunyel]Twitter[/] and [bold link=https://www.linkedin.com/in/bertan-gunyel/]Linkedin"""
 
@@ -25,27 +25,27 @@ tree = Tree("[link=https://www.linkedin.com/in/bertan-gunyel/]Bertan Günyel", g
 projects_tree = tree.add(":open_file_folder: Projects", guide_style="bold")
 tech_stack_tree = tree.add(":computer: Tech Stack", guide_style="bold")
 
-projects_time_series_tree = projects_tree.add(':chart_with_upwards_trend: Time-series')
+projects_cv_tree = projects_tree.add(':man_detective_light_skin_tone: Computer Vision')
 projects_nlp_tree = projects_tree.add(':memo: NLP')
+projects_time_series_tree = projects_tree.add(':chart_with_upwards_trend: Time-series')
 
+projects_cv_tree.add("⭐ [link=https://github.com/bgunyel/cv-fun]CV Fun")
+projects_nlp_tree.add("⭐ [link=https://github.com/bgunyel/nlp-fun]NLP Fun")
 projects_time_series_tree.add("⭐ [link=https://github.com/bgunyel/electricity-load-forecasting]Electricity Load Forecasting")
-# projects_time_series_tree.add("⭐ [link=https://github.com/bgunyel/strava-runners]Strava Runners")
 
-projects_nlp_tree.add("⭐ Coming Soon")
 
-tech_stack_nlp_tree = tech_stack_tree.add(':memo: NLP')
 tech_stack_ml_tree = tech_stack_tree.add(':rocket: Machine Learning')
 tech_stack_ds_tree = tech_stack_tree.add(':dart: Data Science')
 tech_stack_vis_tree = tech_stack_tree.add(':art: Visualization')
+tech_stack_sw_api_tree = tech_stack_tree.add(':computer: Software and API Development')
 tech_stack_mlops_tree = tech_stack_tree.add(':hourglass: ML Ops')
 tech_stack_cloud_tree = tech_stack_tree.add(':crystal_ball: Cloud Computing')
 
-tech_stack_nlp_tree.add('⭐ Hugging Face')
-tech_stack_nlp_tree.add('⭐ NLTK')
-
 tech_stack_ml_tree.add('⭐ PyTorch')
+tech_stack_ml_tree.add('⭐ Hugging Face')
 tech_stack_ml_tree.add('⭐ Scikit-learn')
 
+tech_stack_ds_tree.add('⭐ Polars')
 tech_stack_ds_tree.add('⭐ Pandas')
 tech_stack_ds_tree.add('⭐ Numpy')
 tech_stack_ds_tree.add('⭐ Stats-models')
@@ -53,6 +53,11 @@ tech_stack_ds_tree.add('⭐ Stats-models')
 tech_stack_vis_tree.add('⭐ Matplotlib')
 tech_stack_vis_tree.add('⭐ Seaborn')
 tech_stack_vis_tree.add('⭐ Plotly')
+
+tech_stack_sw_api_tree.add('⭐ Fast API')
+tech_stack_sw_api_tree.add('⭐ Flask')
+tech_stack_sw_api_tree.add('⭐ Pydantic')
+tech_stack_sw_api_tree.add('⭐ SQL Alchemy')
 
 tech_stack_mlops_tree.add('⭐ Domino Data Lab')
 tech_stack_mlops_tree.add('⭐ Weights & Biases')
